e095d0ec61a002097ca3ad0c70295cc249985317
Shivraj Patil <135715287+shivrajpatill@users.noreply.github.com>
2025-04-30 18:41:57 +0530

    Update SQL_Data_Cleaning_Project.sql

--- SQL_Data_Cleaning_Project.sql
+++ SQL_Data_Cleaning_Project.sql
@@ -180,7 +180,85 @@ SELECT*                ----- Cleaned Dataset -----
 FROM layoffs_staging2;
 
 
+-------------------------------------------------------------------------------------------
+--------------------------------- Exploratory Data Analysis -------------------------------
 
+SELECT*
+FROM layoffs_staging2;
+
+
+SELECT MAX(total_laid_off),MAX(percentage_laid_off)
+FROM layoffs_staging2;
+
+SELECT*
+FROM layoffs_staging2
+WHERE percentage_laid_off=1
+ORDER BY funds_raised_millions DESC;
+
+SELECT company,sum(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+SELECT MIN(`date`),MAX(`date`)
+FROM layoffs_staging2;
+
+SELECT industry,SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY industry
+ORDER BY 2 DESC;
+
+
+SELECT country,sum(total_laid_off)
+FROM layoffs_staging2
+GROUP BY country
+ORDER BY 2 DESC;
+
+SELECT*
+FROM layoffs_staging2;
+
+SELECT YEAR (`date`),sum(total_laid_off)
+FROM layoffs_staging2
+GROUP BY YEAR(`date`)
+ORDER BY 1 DESC;
+
+
+SELECT stage,SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY stage
+ORDER BY  2 desc;
+
+
+SELECT company,AVG(percentage_laid_off)
+FROM layoffs_staging2
+GROUP BY company
+ORDER BY 2 DESC;
+
+
+SELECT SUBSTRING(`date`,1,7) AS `MONTH`,sum(total_laid_off)
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC;
+
+
+WITH Rolling_Total AS
+(
+SELECT SUBSTRING(`date`,1,7) AS `MONTH`,SUM(total_laid_off) AS total_off
+FROM layoffs_staging2
+WHERE SUBSTRING(`date`,1,7) IS NOT NULL
+GROUP BY `MONTH`
+ORDER BY 1 ASC
+)
+SELECT `MONTH`,total_off,
+SUM(total_off) OVER(ORDER BY `MONTH`) AS rolling_total
+FROM Rolling_Total;
+
+
+SELECT company, YEAR(`date`), SUM(total_laid_off)
+FROM layoffs_staging2
+GROUP BY company, YEAR(`date`)
+ORDER BY 3 DESC;
 
 
 
